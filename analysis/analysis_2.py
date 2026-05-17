382cdbe499b436393149baa732e4259f2c811166
RaySasa <sasaray33@gmail.com>
2026-05-17 22:38:58 +0900

    いろいろ編集

--- analysis/analysis_2.py
+++ analysis/analysis_2.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 
 #texフォントを使用
 plt.rcParams["text.usetex"] = True
@@ -27,11 +28,11 @@ db_p = np.sqrt(cov_p[1, 1])
 
 #data
 x_ch = np.array([13.5, 21.5, 4.5, 16.5, 20.0, 16.0])#FWHM(ch)
-y_ch = np.array([138, 336, 35, 308, 350, 661.7])#peak(ch)
+y_ch = np.array([138, 336, 35, 308, 350, 178])#peak(ch)
 
 #keVに変換
-x_keV = x_ch * a
-y_keV = y_ch * a + b
+x_keV = x_ch * a_p
+y_keV = y_ch * a_p + b_p
 
 #分解能Rを計算
 R = x_keV / y_keV
@@ -39,13 +40,39 @@ R = x_keV / y_keV
 #\frac{1}{\sqrt{E}}
 Y = 1 / np.sqrt(y_keV)
 
+#最小2乗法(直線)
+(p_l, cov_l) = np.polyfit(Y, R, 1, cov=True)
+a_l = p_l[0]
+b_l = p_l[1]
+da_l = np.sqrt(cov_l[0, 0])
+db_l = np.sqrt(cov_l[1, 1])
+R_lfit = a_l * Y + b_l
+
+#最小2乗法(曲線)
+#フィット関数
+def f(x, a, b):
+    return a /(np.sqrt(x)) + b
+#フィット
+popt, pcov = curve_fit(f, y_keV, R)
+#係数
+a_c = popt[0]
+b_c = popt[1]
+#誤差
+da_c = np.sqrt(pcov[0,0])
+db_c = np.sqrt(pcov[1,1])
+#曲線
+y_cfit = np.linspace(min(y_keV), max(y_keV), 300)
+R_cfit = f(y_cfit, a_c, b_c)
 
 
+#描画
 fig, ax = plt.subplots(1, 2, figsize = (12,5))
 
 ax[0].scatter(y_keV, R, label="data", marker='o', s=20)
+ax[0].plot(y_cfit, R_cfit, label="fit")
 
 ax[1].scatter(Y, R, label="data", marker='o', s=20)
+ax[1].plot(Y, R_lfit)
 
 #ラベル
 for i in range(0,2):
