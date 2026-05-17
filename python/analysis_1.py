4d62ec82dc4e293fa009a9f8b40b94af04fd51a0
RaySasa <sasaray33@gmail.com>
2026-05-17 20:31:50 +0900

    update python

--- python/analysis_1.py
+++ python/analysis_1.py
@@ -8,6 +8,16 @@ import numpy as np
 x = np.array([336, 138, 35, 308, 350])#チャンネル
 y = np.array([1275, 511, 122, 1173, 1333])#エネルギー
 
+# 線源名
+labels = [
+    r"$^{22}\mathrm{Na}$",
+    r"$^{22}\mathrm{Na}$",
+    r"$^{57}\mathrm{Co}$",
+    r"$^{60}\mathrm{Co}$",
+    r"$^{60}\mathrm{Co}$"
+]
+
+
 #最小2乗法
 (p, cov) = np.polyfit(x, y, 1, cov=True)
 
@@ -23,12 +33,38 @@ db = np.sqrt(cov[1, 1])
 y_fit = a * x + b
 
 #描画
-plt.scatter(x, y, label="data")
-plt.plot(x, y_fit, label="fit")
+fit_label = (
+    rf"$y = ({a:.2f} \pm {da:.2f})x + ({b:.0f} \pm {db:.0f})$"
+)
+
+plt.scatter(x, y, label="data", marker='o', s=20)
+
+# 各点にラベル
+
+for i in range(len(x)):
+    if i == 2:
+        plt.annotate(
+            labels[i],
+            (x[i], y[i]),
+            textcoords="offset points",
+         xytext=(-15,10),
+            fontsize=12
+        )
+    else:    
+        plt.annotate(
+            labels[i],
+            (x[i], y[i]),
+            textcoords="offset points",
+         xytext=(-30,0),
+            fontsize=12
+        )
+
+plt.plot(x, y_fit, label=fit_label, linewidth = 1.0)
 
 plt.xlabel("ch")
 plt.ylabel("keV")
 plt.legend()
-plt.grid(linestyle = "--", linewidth =0.5)
+plt.grid(linestyle = "--", linewidth = 0.5)
+
 
 plt.show()
