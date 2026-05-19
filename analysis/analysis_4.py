e050beb49dc7efaa512195724e84e3474904cff1
RaySasa <sasaray33@gmail.com>
2026-05-19 20:20:06 +0900

    角度分布

---
+++ analysis/analysis_4.py
@@ -0,0 +1,20 @@
+#問2 角度分布
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+T = 300
+x = np.array([0, 10, 20, 25, 30, 35, 45, 60, 75])
+y = np.array([11.281, 8.089, 4.967, 1.521, 1.410, 0.678, 0.408, 0.520, 0.408])
+yer = np.array([])
+for i in range(len(y)):
+    if i == 2:
+        yer = np.append(yer, np.sqrt(y[i] * 280) / 280)
+    elif i == 3:
+        yer = np.append(yer, np.sqrt(y[i] *243) / 243)
+    else:
+        yer = np.append(yer, np.sqrt(y[i] * T) / T)
+
+plt.scatter(x, y, label="data", marker='o', s=20)
+plt.show()
