78b497512187d338d7ea2bb9c200cd3605eff97e
RaySasa <sasaray33@gmail.com>
2026-05-21 02:40:14 +0900

    update

--- analysis/analysis_ex2.py
+++ analysis/analysis_ex2.py
@@ -12,7 +12,7 @@ def Om(l, h, R):
 th = np.linspace(0, 75, 300)
 thr = np.radians(th)
 
-l = 5
+L = 5
 Rs = 1.27
 Rb = 2.54
 c = 3
@@ -22,3 +22,16 @@ r = 0.6
 x = np.linspace(- d/2, d/2, 100)
 rho = np.linspace(0, r, 100)
 phi = np.linspace(0, 2 * np.pi, 100)
+
+l = L + x * np.cos(thr) + rho * np.sin(thr) * np.sin(phi)
+h = np.abs(x * np.sin(thr) - rho * np.sin(phi) * np.cos(phi))
+xhp = x + L * np.cos(thr)
+yhp = rho * np.sin(phi) + L * np.sin(thr)
+zhp = rho * np.cos(phi)
+HP = np.sqrt(xhp**2 + yhp**2 + zhp**2)
+yT = yhp * (c + L * np.cos(thr)) / xhp - L * np.sin(thr)
+zT = zhp * (c + L * np.cos(thr)) / xhp
+
+#for i in range(len(x)-1):
+#   for j in range(len(rho)-1):
+#        for k in range(len(phi)-1):
