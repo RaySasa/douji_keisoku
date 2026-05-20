c6ba73496eaa6c646d5d529307daf5da9f6f2d98
RaySasa <sasaray33@gmail.com>
2026-05-20 23:56:12 +0900

    定義

--- analysis/analysis_ex.py
+++ analysis/analysis_ex.py
@@ -7,10 +7,17 @@ from scipy.optimize import curve_fit
 def Om(l,Rs):
     return 2 * np.pi * (1 - l / np.sqrt(Rs**2 + l**2))
 
+th = np.linspace(0, 90, 300)
+thr = th * np.pi / 360
+
+l = 5
+Rs = 1.27
+Rb = 2.54
+Om = Om(l, Rs)
+b = np.sqrt(Om / 2 * np.pi) / np.cos(thr)
+a = b / np.cos(thr)
 
 
 
-
-print(Om(5,2.27))
 
 
