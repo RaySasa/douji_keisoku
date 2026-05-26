ebf420636540a1297d9873d468cedaefbdd45df5
RaySasa <sasaray33@gmail.com>
2026-05-26 19:22:25 +0900

    update

--- analysis/analysis_4.py
+++ analysis/analysis_4.py
@@ -7,6 +7,10 @@ from scipy.optimize import curve_fit
 T = 300
 x = np.array([0, 10, 20, 25, 30, 35, 45, 60, 75])
 y = np.array([11.281, 10.708, 4.967, 1.521, 1.410, 0.678, 0.408, 0.520, 0.408])
+
+x1 = np.array([0, 5, 10, 15, 20, 25, 30, 35, 45, 60])
+y1 = np.array([145.691, 128.762, 94.860, 78.117, 41.344, 15.050, 2.908, 1.127, 0.762, 0.742])
+
 yer = np.array([])
 for i in range(len(y)):
     if i == 2:
@@ -16,6 +20,6 @@ for i in range(len(y)):
     else:
         yer = np.append(yer, np.sqrt(y[i] * T) / T)
 
-plt.scatter(x, y, label="data", marker='o', s=20)
+plt.scatter(x1, y1, label="data", marker='o', s=20)
 plt.savefig("tex/analysis_4.pdf", dpi=300, bbox_inches="tight")
 plt.show()
