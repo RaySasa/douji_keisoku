29e9ad0ac53a72e3972645fd6e21304bf4194478
RaySasa <sasaray33@gmail.com>
2026-06-02 23:43:08 +0900

    update

---
+++ analysis/analysis_7.py
@@ -0,0 +1,29 @@
+#スペクトル取得のプログラム
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_spectrum(filename):
+    with open(filename, "r", encoding="utf-8", errors="ignore") as f:
+        for i, line in enumerate(f):
+            if line.strip() == "[Data]":
+                skiprows = i + 2
+                break
+        else:
+            raise ValueError("[Data] section not found")
+
+    ch, count = np.loadtxt(
+        filename,
+        delimiter=",",
+        skiprows=skiprows,
+        unpack=True
+    )
+
+    return ch, count
+
+ch, count =  load_spectrum("137Cs_spectrum.csv")
+
+plt.plot(ch, count)
+plt.xlabel("ch")
+plt.ylabel("count")
+plt.savefig("../tex/137Cs_spectrum.pdf")
+plt.show()
